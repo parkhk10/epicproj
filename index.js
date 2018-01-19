040d3155fe6037cd393ccc684d7a417ab4e87b73
root <parkhk10@gmail.com>
2018-01-19 03:05:28 -0600

    database/backend changes for joining tables step and task fixed

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+
 app.get('/', function(req, res) { //when you get the / request, respond with the following
 	res.sendFile('index.html');
 })
 
 app.get('/task', function(req, res) { //what you do when you get a request from a client & response
-	db.all("SELECT * FROM task t JOIN step s ON t.id = s.task_id", function(err, tasks) {
+	db.all("SELECT t.id as task_id, name, deadline, t.done as task_done, s.id as step_id, step, in_progress, s.done as step_done FROM task t LEFT JOIN step s ON t.id = s.task_id;", function(err, tasks) {
+    console.log(tasks)
 		if (err) {
 			console.log(err);
 			return res.status(500).json({ message: 'could not retrieve tasks' })
@@ -27,24 +29,35 @@ app.get('/task', function(req, res) { //what you do when you get a request from 
 		var results = {};
 		for (i=0; i<tasks.length; i++) {
 			var task = tasks[i];
-			if (!(task.task_id in results)) {
-				results[task.task_id] = {
+			if (!(task.task_id in results)) { // if task doesnt already exist
+				results[task.task_id] = { // create new object for task
 					task_id: task.task_id,
 					name: task.name,
 					deadline: task.deadline,
-					done: task.done,
+					done: task.task_done,
 					steps: []
 				}
-			}
+      // else { // if it already exists, just append the step to the steps array
+      //   if (task.step != null) {
+      //     results[task.task_id].steps.push({id: task.id, step: task.step, in_progress: task.in_progress, done: task.done})
+      //  }
+      // }
 			// add the step
-			results[task.task_id].steps.push({id: task.id, step: task.step, in_progress: task.in_progress, done: task.done});
-		}
-		res.json(results);
-	});
-})
+			// results[task.task_id].steps.push({id: task.id, step: task.step, in_progress: task.in_progress, done: task.done});
+		  }
+      // for each row in the joined table, as long as the step id is not null, aka as long as it's valid, add it to the steps array of that task
+      if (task.step_id != null) {
+        results[task.task_id].steps.push({id: task.step_id, step: task.step, in_progress: task.in_progress, done: task.step_done})
+      }
+    }
+    console.log(results)
+	  res.json(results);
+  })
+});
 
 app.post('/task', function(req, res) {
 	const task = req.body;
+  console.log(req.body);
 	db.run('INSERT INTO task(name, deadline, done) VALUES($name, $deadline, 0);', {
 		$name: task.name,
 		$deadline: task.deadline
@@ -53,11 +66,13 @@ app.post('/task', function(req, res) {
 			console.error(err);
 			return res.status(500).json({ message: 'Error! Could not create task' });
 		} // `Error! Could not create task ${task.name}` back tick not working for some reason :(
-		console.log('It worked');
+
+    task.id = this.lastID
 		res.status(200).json(task) // anything not 200 is bad. like 404 page error { message: 'Successfully created new task' }
 	});
 })
 
+// post route for adding a step withint a task
 app.post('/task/:id/step', function(req, res) {
 	const step = req.body;
 	db.run('INSERT INTO step(step, done, task_id, in_progress) VALUES($step, $done, $task_id, $in_progress);', {
@@ -69,11 +84,36 @@ app.post('/task/:id/step', function(req, res) {
 		if (err) {
 			console.error(err);
 			return res.status(500).json({ message: 'Error! Could not create step' });
-		} // `Error! Could not create task ${task.name}` back tick not working for some reason :(
+		}
 		console.log('It worked')
 		res.status(200).json({ message: 'Successfully created new step' }) // anything not 200 is bad. like 404 page error
 	});
 })
+
+// app.get('/step', function(req, res) {
+//   db.run('SELECT * FROM step WHERE in_progress = 1);' , function(err, steps) {
+//     if (err){
+// 			console.error(err);
+// 			return res.status(500).json({ message: 'Error! Could not create step' });
+// 		}
+//     res.status(200).json(steps); // all the steps where in_progress is 1
+//   }
+// })
+//
+// updateing in_progress to 1 for steps that have been clicked
+app.put('/update/:id', function(req, res) {
+  db.run('UPDATE step SET in_progress = 1 WHERE id = req.params.id);', function(err, steps) {
+    console.log('put request')
+    if (err) {
+			console.error(err);
+			return res.status(500).json({ message: 'Error! Could not create step' });
+		}
+    res.status(200).json({ message: 'Successfully updated step to in progress' });
+  }
+)
+})
+
+
 
 app.listen(3000, function(){
 	console.log('we\'re listening!');
--- public/tasks.js
+++ public/tasks.js
@@ -1,15 +1,35 @@
-function sendForm(){
-	
-}
-
 $(document).ready(function() {
+	$('#save-button').click(function() {
+		var $newTaskForm = $('#new-task-form');
+		var newTask = $newTaskForm.serialize();
+		$.post('/task', newTask)
+			.done(function(res) {
+				$newTaskForm.trigger('reset')
+				console.log(res)
+				location.reload();
+			})
+			.fail(function(err) {
+				console.log(err)
+			});
+	})
+
+	$.get('/step')
+		.done(function(steps) {
+			var $list = $('<ol/>');
+			$('#in-progress-steps').append($list);
+			for (var i = 0; i < steps.length; i++) {
+				var $item = $('<li/>');
+				$item.text(steps[i].step);
+				$list.append($item);
+			}
+		})
+
 	$.get('/task') // send request, get responses
 		.done(function(tasks) {
 			console.log(tasks)
 			var i = 0;
 			for (taskId in tasks) {
-				var task = tasks[taskId];
-				console.log('its working!');
+				var task = tasks[taskId]
 				// choose the color based on which column you're on
 				// (based on index of the column)
 				var $panel = $('<div/>');
@@ -30,7 +50,7 @@ $(document).ready(function() {
 				$projectBox.addClass('project-box top-box');
 				$projectBox.text(task.name);
 				$panel.append($projectBox);
-
+				// create the first line and the circle that corresponds to it
 				var $line = $('<div/>');
 				$line.addClass('line');
 
@@ -40,20 +60,39 @@ $(document).ready(function() {
 				$panel.append($line);
 
 
-				// don't we have to customize this for each panel? cuz diff # of tasks
+				// rendering the steps that exist for each task
 				for (var j = 0; j < task.steps.length; j++) {
+
 					// create line
 					var $lineNoMargin = $('<div/>');
 					$lineNoMargin.addClass('line-no-margin');
 					// create circle
-					var $circle = $('<div/>');
+					var $circle = $('<button/>');
+					var currentStep = task.steps[j].step_id;
 					$circle.addClass('circle');
+					$circle.attr('id', currentStep);
 					// create the input
 					var $inputTextNext = $('<input/>');
 					// set the color of the input of to match the panel colors
 					$inputTextNext.addClass(panelColor);
 					$inputTextNext.attr('placeholder', task.steps[j].step);
 					$inputTextNext.attr('type', 'text');
+					$inputTextNext.attr('disabled', 'true');
+					$inputTextNext.addClass('step-input');
+					$inputTextNext.attr('id', currentStep);
+					// TODO for each step add click handler to change in_progress to 1 (true).... you need to make a new PUT request
+					//      handler that will update in_progress to true
+					//      $inputTextNext.click(function( $.put(...) ))
+					// LEFT OFF HEREEEEEEEEEEE
+					$circle.click(function(){
+						// stepInProgress should be the value of the input field that has the same class as the clicked button
+						var clickedCircleId = $(this).attr('id');
+						var stepInProgress = $('#' + clickedCircleId).val();
+						console.log(clickedCircleId);
+						console.log(stepInProgress);
+						$.put('/update/' + task.steps[j].step_id, stepInProgress) // i think this should be step_id ... look up doc for .put
+					})
+
 					// add the circle then the input to the line div
 					$lineNoMargin.append($circle);
 					$lineNoMargin.append($inputTextNext);
@@ -61,11 +100,31 @@ $(document).ready(function() {
 					$panel.append($lineNoMargin);
 				};
 
+				// Input box for new step
+
 				var $inputText = $('<input/>');
 				// set the bg of the input to match the panel color
 				$inputText.addClass(panelColor);
 				$inputText.attr('placeholder', 'Text');
 				$inputText.attr('type', 'text');
+				$inputText.attr('id', task.task_id);
+				$inputText.addClass('step-input');
+
+				//TODO: Add keypress handler for new steps that checks if enter is pressed on form and sends POST request
+				// 			a new route that crates a new steps.. see line 76 in index.js
+				//      $inputText.keypress(function(e) { if e.keyCode == whatever the code is for enter { $.post(...) }})
+				$inputText.keypress(function(e) {
+					if(e.which == 13) {
+						var userInputStep = $(this).val();
+						var userinputTaskId = $(this).attr('id');
+						$.post('/task/' + userinputTaskId + '/step', {name: userInputStep})
+						.done(function(res){
+							location.reload();
+							// next: add something here... render the frontend here.
+						})
+					}
+				});
+
 				$line.append($inputText);
 				// POST to "/tasks/taskId/steps"  on enter
 
@@ -83,9 +142,9 @@ $(document).ready(function() {
 				$panel.append($deadline);
 
 				$('#tasks').append($panel); // javascript object that represents html "querying for something in html that already has these things" --aaron leon 2017
-			}
 
-			++i;
+				++i;
+			}
 		})
 		.fail(function(err) {
 			alert('Something went wrong...')
